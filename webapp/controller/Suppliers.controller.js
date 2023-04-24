c89c2557a00f3968d93cfaab6801cc2cd27320c7
srinihog <srinivasa.hog@gmail.com>
2023-04-24 11:52:54 +0000

    Initial commit

---
+++ webapp/controller/Suppliers.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns.businesspartners.controller.Suppliers", {
+            onInit: function () {
+
+            },
+            onButtonPress: function(){
+                var arr1 = [true, true];
+                var arr2 = [1, 2];
+                var arr3 = ['a', 'b'];
+                this.callBackFunction({a:1,b:2,c:3},'b')
+            },
+            callBackFunction: function(a,b){
+
+            }
+        });
+    });
